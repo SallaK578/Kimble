3f15f65dbe01625a9306d8d2a7859f198838e4d2
Salla Koski <salla.k.koski@utu.fi>
2021-03-29 13:15:02 +0300

    kuvia poistettu

--- src/main/java/fi/utu/tech/gui/javafx/MainApp.java
+++ src/main/java/fi/utu/tech/gui/javafx/MainApp.java
@@ -13,7 +13,7 @@ import javafx.stage.Stage;
 public class MainApp extends Application{
     Stage stage;
 
-    
+
 
     public void start(Stage stage) { //Throws exception
         this.stage = stage;
